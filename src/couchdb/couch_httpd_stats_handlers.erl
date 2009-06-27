851d562fa4fd801551f858636f54736642e5a004
Paul J. Davis <paul.joseph.davis@gmail.com>
2009-06-27 17:04:59 -0400

    Fixing up the couch_stats_aggregator module.

--- src/couchdb/couch_httpd_stats_handlers.erl
+++ src/couchdb/couch_httpd_stats_handlers.erl
@@ -12,7 +12,6 @@
 
 -module(couch_httpd_stats_handlers).
 -include("couch_db.hrl").
--include("couch_stats.hrl").
 
 -export([handle_stats_req/1]).
 -import(couch_httpd,
@@ -20,28 +19,13 @@
     start_json_response/2,send_chunk/2,end_json_response/1,
     start_chunked_response/3, send_error/4]).
 
--define(b2a(V), list_to_atom(binary_to_list(V))).
-
--record(stats_query_args, {
-    range='0',
-    flush=false
-}).
 
 handle_stats_req(#httpd{method='GET', path_parts=[_]}=Req) ->
-    send_json(Req, couch_stats_aggregator:all());
+    Range = get_range(Req),
+    send_json(Req, couch_stats_aggregator:all(Range));
 
 handle_stats_req(#httpd{method='GET', path_parts=[_Stats, Module, Key]}=Req) ->
-    #stats_query_args{
-        range=Range,
-        flush=Flush
-    } = parse_stats_query(Req),
-
-    case Flush of
-        true ->
-            couch_stats_aggregator:time_passed();
-        _ -> ok
-    end,
-
+    Range = get_range(Req),
     Stats = couch_stats_aggregator:get_json({?b2a(Module), ?b2a(Key)}, Range),
     Response = {[{Module, {[{Key, Stats}]}}]},
     send_json(Req, Response);
@@ -49,14 +33,8 @@ handle_stats_req(#httpd{method='GET', path_parts=[_Stats, Module, Key]}=Req) ->
 handle_stats_req(Req) ->
     send_method_not_allowed(Req, "GET").
 
-parse_stats_query(Req) ->
-    lists:foldl(fun({Key,Value}, Args) ->
-        case {Key, Value} of
-        {"range", Range} ->
-            Args#stats_query_args{range=list_to_atom(Range)};
-        {"flush", "true"} ->
-            Args#stats_query_args{flush=true};
-        _Else -> % unknown key value pair, ignore.
-            Args
-        end
-    end, #stats_query_args{}, couch_httpd:qs(Req)).
+get_range(Req) ->
+    case proplists:lookup("range", couch_httpd:qs(Req)) of
+        none -> 0;
+        {_, Value} -> list_to_integer(Value)
+    end.
